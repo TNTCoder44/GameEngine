a79febd06713339bca1ba51602fa8347d7817185
Janis Gätzi <janis.gaetzi@icloud.com>
2023-12-21 13:35:13 +0100

    camera system

---
+++ src/Application.cpp
@@ -0,0 +1,260 @@
+#ifdef _WIN32
+#ifndef _DEBUG
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+#endif
+
+#include "Includes.h"
+
+int main(int argc, char **argv)
+{
+    GLFWwindow *window;
+
+    /* Initialize GLFW */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // find primary monitor
+    auto *monitor = glfwGetPrimaryMonitor();
+
+    /* Create a windowed mode window and its OpenGL context */
+    // change monitor variable to monitor to get fullscreen resolution
+    window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "OpenGl", nullptr, nullptr);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    // handle resizing
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // handle mouse input
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    // handle scrolling input
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // uncomment to set window fullscreen resolution
+    // glfwSetWindowMonitor(window, glfwGetPrimaryMonitor(), 0, 0, 1920, 1080, 280);
+
+    // wait for vsync
+    glfwSwapInterval(1);
+
+    // Set Window Icon
+    SetWindowIcon(window, "../res/icons/window.png");
+
+    // init glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // initialize ImGui
+    ImGui::CreateContext();
+    ImGui_ImplGlfwGL3_Init(window, true);
+    ImGui::StyleColorsDark();
+
+    // start coding
+
+    GLfloat vertices[] = {
+         // positions         // colors          // texture coords
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+
+        -0.5f, -0.5f, 0.5f,   1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
+         0.5f, -0.5f, 0.5f,   0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f, 0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+         0.5f,  0.5f, 0.5f,   1.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+        -0.5f,  0.5f, 0.5f,   0.0f, 1.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, 0.5f,   1.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+
+        -0.5f, 0.5f, -0.5f,   1.0f, 0.0f, 0.0f,  0.0f, 1.0f,
+         0.5f, 0.5f, -0.5f,   0.0f, 1.0f, 0.0f,  1.0f, 1.0f,
+         0.5f, 0.5f,  0.5f,   0.0f, 0.0f, 1.0f,  1.0f, 0.0f,
+         0.5f, 0.5f,  0.5f,   1.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+        -0.5f, 0.5f,  0.5f,   0.0f, 1.0f, 1.0f,  0.0f, 0.0f,
+        -0.5f, 0.5f, -0.5f,   1.0f, 0.0f, 1.0f,  0.0f, 1.0f
+    };
+
+    glm::vec3 cubePositions[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3(2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f, 3.0f, -7.5f),
+        glm::vec3(1.3f, -2.0f, -2.5f),
+        glm::vec3(1.5f, 2.0f, -2.5f),
+        glm::vec3(1.5f, 0.2f, -1.5f),
+        glm::vec3(-1.3f, 1.0f, -1.5f)};
+
+    GLuint indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    // GLCall(glEnable(GL_BLEND));
+    // GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
+
+    GLCall(glEnable(GL_DEPTH_TEST));
+
+    // Shader
+    Shader shader("../res/shaders/shader.vert", "../res/shaders/shader.frag", "");
+    shader.Bind();
+
+    // VertexArray, VertexBuffer, IndexBuffer
+    VertexArray vao;
+    VertexBuffer vbo(vertices, sizeof(vertices));
+    IndexBuffer ibo(indices, sizeof(indices));
+    VertexBufferLayout layout;
+
+    vao.Bind();
+    vbo.Bind();
+    ibo.Bind();
+
+    // Textures
+    Texture texture1("../res/textures/container.jpg", false, GL_TEXTURE_2D,
+                     GL_RGB, GL_REPEAT, GL_LINEAR, GL_LINEAR, 0);
+    Texture texture2("../res/textures/awesomeface.png", true, GL_TEXTURE_2D,
+                     GL_RGBA, GL_REPEAT, GL_LINEAR, GL_LINEAR, 0);
+
+    texture1.Bind(0);
+    texture2.Bind(1);
+
+    // Positions
+    layout.Push<float>(3, GL_FALSE);
+    // Colors
+    layout.Push<float>(3, GL_FALSE);
+    // Textures
+    layout.Push<float>(2, GL_FALSE);
+
+    vao.AddBuffer(vbo, layout);
+
+    Renderer renderer;
+
+    // unbind everything
+    vao.Unbind();
+    vbo.Unbind();
+    ibo.Unbind();
+    shader.Unbind();
+    texture1.Unbind();
+    texture2.Unbind();
+
+    float mixValue = 0.2f;
+
+    // main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // calculate time logic
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // check for input
+        renderer.processInput(window, mixValue);
+
+        // init ImGui
+        ImGui_ImplGlfwGL3_NewFrame();
+
+        /* start rendering */
+        // clear background
+        renderer.Clear(glm::vec4(0.0f, 0.0f, 0.0f, 1.0f));
+
+        // calculate position
+        // view matrix
+        glm::mat4 view = glm::mat4(1.0f);
+        view = camera.GetViewMatrix();
+
+        // projection matrix
+        glm::mat4 proj = glm::mat4(1.0f);
+        proj = glm::perspective(glm::radians(camera.Zoom),
+                                static_cast<float>(WINDOW_WIDTH) / static_cast<float>(WINDOW_HEIGHT),
+                                0.1f, 100.0f);
+
+        // render screen
+        texture1.Bind(0);
+        texture2.Bind(1);
+        shader.Bind();
+        shader.SetUniform1i("texture1", 0);
+        shader.SetUniform1i("texture2", 1);
+        shader.SetUniform1f("mixValue", mixValue);
+        shader.SetUniformMat4("view", view);
+        shader.SetUniformMat4("projection", proj);
+        renderer.Draw(vao, ibo, shader);
+
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            angle = glfwGetTime() * 25.0f;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            shader.SetUniformMat4("model", model);
+            renderer.Draw(vao, shader, 36);
+        }
+
+        // draw ImGui
+        // renderer.OnImGuiRender();
+
+        // render ImGui
+        ImGui::Render();
+        ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // do glfw stuff
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    vao.Delete();
+    vbo.Delete();
+    ibo.Delete();
+    shader.Delete();
+    texture1.Delete();
+    texture2.Delete();
+
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+
+    return 0;
+}
---
+++ src/Globals.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "Camera.h"
+
+inline constexpr int WINDOW_WIDTH = 1000;
+inline constexpr int WINDOW_HEIGHT = 800;
+
+inline bool firstMouse = true;
+
+inline float deltaTime = 0.0f;
+inline float lastFrame = 0.0f;
+
+
