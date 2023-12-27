ff3eadf29ef244ed5b5a5c7ca00006abbe9b5c6f
Janis Gätzi <janis.gaetzi@icloud.com>
2023-12-27 17:03:05 +0100

    adding libsndfile to the project for openal

--- src/SoundEngine.cpp
+++ src/SoundEngine.cpp
@@ -14,9 +14,39 @@ SoundEngine::~SoundEngine()
     alcCloseDevice(device);
 }
 
+void SoundEngine::LoadSound(const char* filePath, ALuint& buffer)
+{
+    SNDFILE* sndfile;
+    SF_INFO sfinfo;
+    memset(&sfinfo, 0, sizeof(sfinfo));
+
+    // Öffne die Sounddatei
+    sndfile = sf_open(filePath, SFM_READ, &sfinfo);
+    if (!sndfile) {
+        // Fehler beim Öffnen der Datei
+        return;
+    }
+
+    // Lese die Sounddaten
+    ALsizei size = static_cast<ALsizei>(sfinfo.frames) * sfinfo.channels;
+    ALsizei freq = sfinfo.samplerate;
+
+    short* membuf = new short[size];
+    sf_readf_short(sndfile, membuf, size);
+
+    // Schließe die Sounddatei
+    sf_close(sndfile);
+
+    // Erzeuge einen OpenAL-Buffer und kopiere die Daten
+    alGenBuffers(1, &buffer);
+    alBufferData(buffer, AL_FORMAT_MONO16, membuf, size * sizeof(short), freq);
+
+    delete[] membuf;
+}
+
 void SoundEngine::PlaySound(const char* filePath) {
     ALuint buffer;
-    alGenBuffers(1, &buffer);
+    LoadSound(filePath, buffer);
 
     ALuint source;
     alGenSources(1, &source);
--- src/SoundEngine.h
+++ src/SoundEngine.h
@@ -3,11 +3,16 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 
+#include <sndfile.h>
+
+#include <string>
+
 class SoundEngine {
 public:
     SoundEngine();
     ~SoundEngine();
 
+    void LoadSound(const char* filePath, ALuint& buffer);
     void PlaySound(const char* filePath);
 
 private:
